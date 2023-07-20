7e2477508d94ceb2569015facfe5f7ef3ba26623
Parth Shah <87560178+codingis4noobs2@users.noreply.github.com>
2023-07-20 19:08:14 +0530

    Update Chat_UI.py

--- Chat_UI.py
+++ Chat_UI.py
@@ -5,9 +5,9 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def display_msg(text, is_user='Ash'):
+def display_message(text, is_user='Ash'):
     """
-    This function is used to display the messages UI.
+    This function is used to display the message UI.
 
     Parameters:
     text (str): The text to be displayed.
@@ -22,7 +22,8 @@ def display_msg(text, is_user='Ash'):
             f"""
             <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
                 <div style="background: {message_bg_color}; color: white; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%; font-size: 14px; border: 1px solid #ccc;">
-                    {text} \n </div>
+                    {text}
+                </div>
                 <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
             </div>
             """,
@@ -31,7 +32,7 @@ def display_msg(text, is_user='Ash'):
         st.caption(
             f"""
             <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
-                <div style="color: white; margin-right: 5px;">Ash\n</div>
+                <div style="color: white; margin-right: 5px;">Ash</div>
             </div>
             """,
             unsafe_allow_html=True
@@ -47,7 +48,8 @@ def display_msg(text, is_user='Ash'):
             <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
                 <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
                 <div style="background: {message_bg_color}; color: white; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%; font-size: 14px; border: 1px solid #ccc;">
-                    {text} \n </div>
+                    {text}
+                </div>
             </div>
             """,
             unsafe_allow_html=True,
@@ -55,22 +57,22 @@ def display_msg(text, is_user='Ash'):
         st.caption(
             f"""
             <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
-                <div style="color: white; margin-right: 5px;">May\n</div>
+                <div style="color: white; margin-right: 5px;">May</div>
             </div>
             """,
             unsafe_allow_html=True
         )
 
 def display_chat(df):
-    # convo 1
-    display_msg("Hey Ash👋🏼, I got this image from Misty. I was wondering what you were doing with those graphs and charts?🤔", "May")
+    # Conversation 1
+    display_message("Hey Ash👋🏼, Misty sent me this image. I was wondering what you were doing with those graphs and charts?🤔", "May")
     st.image("https://i.ibb.co/Tq1JrvJ/Ash-doing-Analysis.png")
-    display_msg("Hey May👋🏼, I was working on the Pokémon dataset. It's part of the prestigious Angelhack Monthly Challenge Series and it has an overall prize pool of 5000 Pokémon Dollars💵", "Ash")
-    display_msg("That's great, Ash. I really want to learn more about this dataset. It will help with our exploration too.", "May")
-    display_msg("Yes, you're right May. This will help in our exploration.", "Ash")
-    display_msg("But I know very little about data analysis😭", "May")
-    display_msg("HAHAHA, no worries May.", "Ash")
-    display_msg("Let's start by loading the dataset. Don't worry May, if you don't know how to do this, I have included the code for you.", "Ash")
+    display_message("Hey May👋🏼, I was working on the Pokémon dataset. It's part of the prestigious Angelhack Monthly Challenge Series, and it has an overall prize pool of 5000 Pokémon Dollars💵", "Ash")
+    display_message("That's great, Ash. I'm really interested in learning more about this dataset. It will also help us with our exploration.", "May")
+    display_message("Yes, you're right May. This dataset will definitely aid in our exploration.", "Ash")
+    display_message("But I know very little about data analysis😭", "May")
+    display_message("HAHAHA, no worries May.", "Ash")
+    display_message("Let's start by loading the dataset. Don't worry, if you don't know how to do this, I have included the code for you.", "Ash")
     tab1, tab2 = st.tabs(['Dataset', 'Code'])
     with tab1:
         st.dataframe(df, height=220)
@@ -87,11 +89,11 @@ def display_chat(df):
             """,
             language="python"
         )
-    display_msg("Omg, this is a very big dataset😱. How are we going to do analysis on such a big dataset?", "May")
-    display_msg("The library I used to load the data & create a dataframe from this dataset is Pandas, The reason why pandas is widely used is it offers a huge number of useful methods to deal with Data")
-    display_msg("That's great Ash, Got to learn something new today😊", "May")
-    display_msg("HAHAHA, Enough Talking now let's jump right into the World of Data")
-    display_msg("Let's start by getting a raw information about the dataset")
+    display_message("Wow, this dataset is quite extensive😱. How are we going to perform analysis on such a large dataset?", "May")
+    display_message("The library I used to load the data and create a DataFrame from this dataset is Pandas. Pandas is widely used because it offers a vast number of useful methods to deal with data.", "Ash")
+    display_message("That's great, Ash! I've learned something new today😊", "May")
+    display_message("HAHAHA, enough talking. Let's jump right into the world of data!", "Ash")
+    display_message("Let's start by getting some basic information about the dataset.", "Ash")
     tab1, tab2 = st.tabs(['Results', 'Code'])
     with tab1:
         buffer = io.StringIO()
@@ -107,13 +109,13 @@ def display_chat(df):
             """,
             language="python"
         )
-    display_msg("It has 13 Columns & 1194 Rows, Additionally 9 Columns have int datatype and 4 Columns has object datatype")
-    display_msg("But i intentionally left 1 detail, Can you think of it?")
-    display_msg("Yes yes, I noticed it the Type-2 Column has some null values in it", "May")
-    display_msg("Great work over there May👏🏻. It is important to clean data before we start working on it")
-    display_msg("But in this it is not important to replace Null, we will simply ignore the value if its Null")
-    display_msg("Ohh Great, Well i was wondering where do we start from?", "May")
-    display_msg("We can start by analyzing the types of Pokemons, Let's make a Piechart to better understand the distribution of types in Type-1 and Type-2")
+    display_message("The dataset consists of 13 columns and 1194 rows. Additionally, 9 columns have an integer datatype, and 4 columns have an object datatype.", "Ash")
+    display_message("But I intentionally left out one detail. Can you guess what it is?", "Ash")
+    display_message("Yes, yes! I noticed it. The Type-2 column has some null values in it.", "May")
+    display_message("Great work, May👏🏻! It is important to clean the data before we start working with it.", "Ash")
+    display_message("In this case, it is not necessary to replace the null values. We will simply ignore them.", "Ash")
+    display_message("Ohh, great! Well, I was wondering where do we start from?", "May")
+    display_message("We can start by analyzing the types of Pokémon. Let's create a pie chart to better understand the distribution of types in Type-1 and Type-2.", "Ash")
     tab1, tab2 = st.tabs(['Results', 'Code'])
     with tab1:
         type1_counts = df['Type1'].value_counts()
@@ -126,19 +128,19 @@ def display_chat(df):
         st.plotly_chart(fig)
     with tab2:
         st.code(
-        """
-        type1_counts = df['Type1'].value_counts()
-        type2_counts = df['Type2'].value_counts()
+            """
+            type1_counts = df['Type1'].value_counts()
+            type2_counts = df['Type2'].value_counts()
 
-        fig = px.pie(values=type1_counts.values, names=type1_counts.index, title='Type 1 Distribution')
-        st.plotly_chart(fig)
+            fig = px.pie(values=type1_counts.values, names=type1_counts.index, title='Type 1 Distribution')
+            st.plotly_chart(fig)
 
-        fig = px.pie(values=type2_counts.values, names=type2_counts.index, title='Type 2 Distribution')
-        st.plotly_chart(fig)
-        """
-    )
-    display_msg("Ohh This Visualization makes it very easy to identify the Flying is the most common type of both Type-1 and Type-2", "May")
-    display_msg("Good Oberservation May, Now lets focus on the Pokemon stats distribution")
+            fig = px.pie(values=type2_counts.values, names=type2_counts.index, title='Type 2 Distribution')
+            st.plotly_chart(fig)
+            """
+        )
+    display_message("Ohh, this visualization makes it very easy to identify that Flying is the most common type in both Type-1 and Type-2.", "May")
+    display_message("Good observation, May! Now, let's focus on the distribution of Pokémon stats.", "Ash")
     stat = st.selectbox("Choose a stat to visualize", ['Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'])
     tab1, tab2 = st.tabs(['Result', 'Code'])
     with tab1:
@@ -159,27 +161,27 @@ def display_chat(df):
             plt.show()
             """
         )
-    display_msg("Omg i didn't knew that this libraries can be too useful", "May")
-    display_msg("But it is still hard for me to understand this as if i want to know that X% Pokemons have better stat than a given value, how could i know that?", "May")
-    display_msg("Don't worry, I gotch you")
-    stat = st.selectbox("Choose a stat to get X% of pokemon having better stat than given", ['Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'])
+    display_message("Wow, I didn't know that these libraries could be so useful.", "May")
+    display_message("But it's still hard for me to understand. If I want to know what percentage of Pokémon have better stats than a given value, how can I find that out?", "May")
+    display_message("Don't worry, I've got you covered.", "Ash")
+    stat = st.selectbox("Choose a stat to get the percentage of Pokémon having better stats than a given value", ['Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'])
     start = int(df[stat].min())
     end = int(df[stat].max())
     x = st.slider("Select X", min_value=start, max_value=end)
     total_pokemon = df.shape[0]
     higher_total_pokemon = df[df[stat] > x].shape[0]
-    st.write(f"{higher_total_pokemon} Pokemon have better {stat} than {x} {stat}")
-    display_msg("Impressive! With your help, I can now determine the number of Pokémon that possess a higher value than the given benchmark.", "Msg")
-    display_msg("Waitt!!, How can we forget the most basic ones😲")
-    display_msg("Huh, What happened Ash?", "May")
-    display_msg("We will get the top5 Strongest & Weakest Pokemon by their stats, Let me show you how")
-    stat = st.selectbox("Choose a stat to get 5 Pokemons with highest/lowest stat", ['Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'])
+    st.write(f"{higher_total_pokemon} Pokémon have better {stat} than {x} {stat}")
+    display_message("Impressive! With your help, I can now determine the number of Pokémon that possess a higher value than the given benchmark.", "Ash")
+    display_message("Wait! How can we forget the basics?", "May")
+    display_message("Huh? What happened, Ash?", "May")
+    display_message("We can get the top 5 strongest and weakest Pokémon based on their stats. Let me show you how.", "Ash")
+    stat = st.selectbox("Choose a stat to get the 5 Pokémon with the highest/lowest stats", ['Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'])
     highest_total = df.nlargest(5, stat)[['Names', stat]]
     lowest_total = df.nsmallest(5, stat)[['Names', stat]]
     st.write(highest_total)
-    display_msg("That's great, I am learning soo much today!!", "May")
-    display_msg("Let's analyze the pokemon stats using a radar char")
-    pokemon_name = st.selectbox("Select a Pokemon to analyze", df['Names'])
+    display_message("That's great! I'm learning so much today!!", "May")
+    display_message("Let's analyze the Pokémon stats using a radar chart.", "Ash")
+    pokemon_name = st.selectbox("Select a Pokémon to analyze", df['Names'])
     pokemon = df[df['Names'] == pokemon_name]
     if not pokemon.empty:
         hp = pokemon['HP'].values[0]
@@ -188,52 +190,7 @@ def display_chat(df):
         sp_atk = pokemon['Sp. Atk'].values[0]
         sp_def = pokemon['Sp. Def'].values[0]
         speed = pokemon['Speed'].values[0]
-    fig = px.line_polar(df, r=[hp, attack, defense, sp_atk, sp_def, speed], theta=['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'], line_close=True)
-    fig.update_traces(fill='toself')
-    st.plotly_chart(fig)
-    display_msg("As we have a stat looker for a individual pokemon why not make a Pokemon comparison looker too")
-    col1, col2 = st.columns(2)
-    with col1:
-        pokemon_1 = st.selectbox("Select First Pokemon to analyze", df['Names'])
-    with col2:
-        pokemon_2 = st.selectbox("Select Second Pokemon to analyze", df['Names'])
-    pokemon1 = df[df['Names'] == pokemon_2]
-    if not pokemon.empty:
-        hp1 = pokemon['HP'].values[0]
-        attack1 = pokemon['Attack'].values[0]
-        defense1 = pokemon['Defense'].values[0]
-        sp_atk1 = pokemon['Sp. Atk'].values[0]
-        sp_def1 = pokemon['Sp. Def'].values[0]
-        speed1 = pokemon['Speed'].values[0]
-    pokemon2 = df[df['Names'] == pokemon_2]
-    if not pokemon.empty:
-        hp2 = pokemon['HP'].values[0]
-        attack2 = pokemon['Attack'].values[0]
-        defense2 = pokemon['Defense'].values[0]
-        sp_atk2 = pokemon['Sp. Atk'].values[0]
-        sp_def2 = pokemon['Sp. Def'].values[0]
-        speed2 = pokemon['Speed'].values[0]
-    fig3 = go.Figure()
-
-    fig3.add_trace(go.Scatterpolar(
-        r=[hp1, attack1, defense1, sp_atk1, sp_def1, speed1],
-        theta=['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'],
-        fill='toself',
-        name=pokemon_1
-    ))
-    fig3.add_trace(go.Scatterpolar(
-        r=[hp2, attack2, defense2, sp_atk2, sp_def2, speed2],
-        theta=['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'],
-        fill='toself',
-        name=pokemon_2
-    ))
-
-    fig3.update_layout(
-    polar=dict(
-        radialaxis=dict(
-        visible=True,
-        range=[0, 5]
-        )),
-    showlegend=False
-    )
-    st.plotly_chart(fig3)
+        fig = px.line_polar(df, r=[hp, attack, defense, sp_atk, sp_def, speed], theta=['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'], line_close=True)
+        fig.update_traces(fill='toself')
+        st.plotly_chart(fig)
+    display_message("As we have a stat looker for individual Pokémon, why not create a Pokémon comparison looker too?", "Ash")
